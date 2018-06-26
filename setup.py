824758f9e0657f9bb65dea8b3d92d44052948c78
Graham Rowlands <grahamrow@gmail.com>
2018-06-26 09:28:34 -0400

    Add requirements file and update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,13 @@ from setuptools import setup, find_packages
 setup(name='QGL',
       version='2.1',
       url='https://github.com/BBN-Q/QGL',
-      packages=find_packages(exclude=["tests"]))
+      packages=find_packages(exclude=["tests"]),
+      install_requires=[
+        "bbndb >= 0.1",
+        "numpy >= 1.11.1",
+        "scipy >= 0.17.1",
+        "networkx >= 1.11",
+        "h5py >= 2.6.0",
+        "bokeh >= 0.12.13",
+        # "pony >= 0.7.4", # This needs to be 0.7.4-dev
+    ])
